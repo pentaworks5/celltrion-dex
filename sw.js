5d8d1365334c1eb80e005d3e665214c7305bef1d
pentaworks5 <uranusn5@gmail.com>
2026-03-12 15:40:08 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+// Service Worker - 캐시 완전 무력화
+const VERSION = '20260312154500';
+
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request, { cache: 'no-store' })
+      .catch(() => caches.match(e.request))
+  );
+});
