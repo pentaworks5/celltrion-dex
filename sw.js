a1847cecfa85a19bb12d53f281755a966fa3ba81
pentaworks5 <uranusn5@gmail.com>
2026-03-16 16:20:24 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+// Service Worker 자동 제거 - 기존 캐시 완전 삭제
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll())
+      .then(clients => clients.forEach(c => c.navigate(c.url)))
+  );
+});
